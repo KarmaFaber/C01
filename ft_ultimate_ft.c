09f1cd344000500ce4f32b1dd97abdc69b1aa5c7
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-05-29 20:17:24 +0200

    ft_ultimate_ft.c

---
+++ ft_ultimate_ft.c
@@ -0,0 +1,42 @@
+void    ft_ultimate_ft(int *********nbr)
+{
+  *********nbr = 42;  
+}
+
+
+
+/*
+#include <stdio.h>
+int     main() 
+{
+    
+    int a;
+    
+    int *nbr8;
+    int **nbr7;
+    int ***nbr6;
+    int ****nbr5;
+    int *****nbr4;
+    int ******nbr3;
+    int *******nbr2;
+    int ********nbr1;
+    int *********nbr;
+    
+    a = 10;
+    
+    nbr8 = &a;
+    nbr7 = &nbr8;
+    nbr6 = &nbr7;
+    nbr5 = &nbr6;
+    nbr4 = &nbr5;
+    nbr3 = &nbr4;
+    nbr2 = &nbr3;
+    nbr1 = &nbr2;
+    nbr = &nbr1;
+    
+    
+    ft_ultimate_ft(nbr);
+    printf("el valor es %d", a);
+    
+    return 0;
+}*/
