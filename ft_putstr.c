87acdb73c5d404f6557ce9b5ab10b9cb2925bc9f
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-05-30 15:35:33 +0200

    Create ft_putstr.c

---
+++ ft_putstr.c
@@ -0,0 +1,22 @@
+void  ft_putstr(char *str)
+{
+    int count;
+    count = 0;
+    
+    while (str[count] != '\0')
+    {
+        write(1, &str[count], 1);
+        count++;
+    }
+}
+
+
+/*#include <stdio.h>
+int main() {
+    
+    char *mystr = "Hello world!";
+
+    ft_putstr(mystr);
+
+    return 0;
+}*/
