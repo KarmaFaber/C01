2e023e703884bb2524186707527dc41de96a4eaa
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-05-30 09:42:37 +0200

    Update ft_ft.c

--- ft_ft.c
+++ ft_ft.c
@@ -1,19 +1,14 @@
-
-
 void	ft_ft(int *nbr)
 {
 	*nbr = 42;
-/*
-
-#include <stdio>
-
 }
+/*#include <stdio.h>
 
 int	main(void)
 {
 	int a; // declaramos la varieble donde gusrdaremos el numero
 	int *dir_a; //declaramos un puntero dir_a que apuntara  a un entero
-	dir_a=&a; // asignamos la direccion de la varieble 'a' al puntero 'dir_a'. Esto hace que 'dir_a' ahora punte a 'a'
+	dir_a=&a; // asignamos la direccion de la varieble 'a' al puntero 'dir_a'. Esto hace que 'dir_a' ahora puntee a 'a'
 	ft_ft(dir_a); // llamos  la funcion creada y le pasamos la dirección de 'a'
 	printf("el numemero es %d", a);
 }*/
