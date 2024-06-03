bd60bbbcce2335e7b88536c1f0ae8a16b5a805d7
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-06-03 14:03:00 +0200

    ex07

---
+++ ft_rev_int_tab.c
@@ -0,0 +1,35 @@
+
+
+void ft_rev_int_tab (int *tab, int size)
+{ 
+    int i; // counter
+
+    i = 0;
+    while (i < size/2)
+    {
+        ft_swap(&tab[i], &tab[size-1-i]);
+        i ++;
+    }  
+}
+
+#include <stdio.h>
+int main()
+{
+    int tab[9] = {2, 5, 7, 9, 10, 12, 16, 24, 777};
+    printf("\nla array original es:\n");
+    int i=0;
+    while (i<9){
+
+        printf("%d ", tab[i]);
+        i ++ ; 
+    }
+    ft_rev_int_tab(tab, 9);
+    printf("\nla array invertida es:\n");
+    i=0;
+    while (i<9){
+
+        printf("%d ", tab[i]);
+        i ++ ; 
+    }
+    return 0;
+}
